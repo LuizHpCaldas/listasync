700d6b1dd928e8a24cd6e87024c1a0fa1a6e6b55
luizhpcaldas <luizhpcaldas@gmail.com>
2026-05-28 13:54:47 -0300

    feat: implement Supabase client in middleware for enhanced authentication handling

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,63 @@
-import { NextResponse } from "next/server";
+import { createServerClient } from "@supabase/ssr";
 
-import type { NextRequest } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
-  const token = request.cookies.get("sb-access-token");
+export async function middleware(request: NextRequest) {
+  let response = NextResponse.next({
+    request,
+  });
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name) {
+          return request.cookies.get(name)?.value;
+        },
+
+        set(name, value, options) {
+          request.cookies.set({
+            name,
+            value,
+            ...options,
+          });
+
+          response = NextResponse.next({
+            request,
+          });
+
+          response.cookies.set({
+            name,
+            value,
+            ...options,
+          });
+        },
+
+        remove(name, options) {
+          request.cookies.set({
+            name,
+            value: "",
+            ...options,
+          });
+
+          response = NextResponse.next({
+            request,
+          });
+
+          response.cookies.set({
+            name,
+            value: "",
+            ...options,
+          });
+        },
+      },
+    },
+  );
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
   const isAuthPage = request.nextUrl.pathname.startsWith("/login");
 
@@ -12,15 +66,15 @@ export function middleware(request: NextRequest) {
     request.nextUrl.pathname.startsWith("/lists") ||
     request.nextUrl.pathname.startsWith("/shared");
 
-  if (!token && isProtectedRoute) {
+  if (!user && isProtectedRoute) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
 
-  if (token && isAuthPage) {
+  if (user && isAuthPage) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
-  return NextResponse.next();
+  return response;
 }
 
 export const config = {
